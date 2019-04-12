434c30ea9e67fa1795500496bfeafe8fcd9436dd
truongduy5998 <truongduy5998@gmail.com>
2019-04-12 22:46:50 +0700

    Add section slider

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,33 @@
-console.log('Run');
+jQuery(function($) {
+	"use strict";
+
+	var myClass = window.myClass || {};
+	
+
+	/*==========================
+	=		MAIN FUNCTION      =
+	============================*/
+		myClass.sliderFunction = function() {
+			if( $('.slider').length ) {
+				$('.slider').slick({
+					infinite: true,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: true,
+					arrows: false
+				});
+			}
+		};
+
+
+
+	/*==========================
+	=		INIT FUNCTION      =
+	============================*/
+		$(document).ready(function() {
+			myClass.sliderFunction();
+		});
+	
+		$(window).on('load', function() {});
+		$(window).on('resize', function() {});
+});

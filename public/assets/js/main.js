ed9a865fe225a0d0f52c4aa2a1c5604cd1b326e9
truongduy5998 <truongduy5998@gmail.com>
2019-04-13 22:28:14 +0700

    Add section introduce

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,24 +8,55 @@ jQuery(function($) {
 	=		MAIN FUNCTION      =
 	============================*/
 		myClass.sliderFunction = function() {
-			if( $('.slider').length ) {
-				$('.slider').slick({
-					infinite: true,
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					dots: true,
-					arrows: false
+			if( $('.comp-slider').length ) {
+				$('.comp-slider').each(function() {
+					var mSlidesToShow = parseInt($(this).attr('data-slidesToShow')) || 1;
+					var mSlidesToScroll = parseInt($(this).attr('data-slidesToScroll')) || 1;
+					var mDots = parseInt($(this).attr('data-dots')) || 0;
+					var mArrows = parseInt($(this).attr('data-arrows')) || 0;
+
+					mDots = !!mDots;
+					mArrows = !!mArrows;
+
+					$(this).slick({
+						infinite: true,
+						slidesToShow: mSlidesToShow,
+						slidesToScroll: mSlidesToScroll,
+						dots: mDots,
+						arrows: mArrows
+					});
+
+					console.log(mSlidesToShow);
+					console.log(typeof mSlidesToShow);
+					console.log(mSlidesToScroll);
+					console.log(typeof mSlidesToScroll);
+					console.log(mDots);
+					console.log(typeof mDots);
+					console.log(mArrows);
+					console.log(typeof mArrows);
 				});
+
 			}
 		};
 
-
+		myClass.setHeight = function() {
+			if( $('.intro__item').length ) {
+				var heightValue = 0;
+				$('.intro__item').each(function() {
+					if($(this).outerHeight() > heightValue) {
+						heightValue = $(this).outerHeight();
+					}
+				});
+				$('.intro__item').css('height', heightValue);
+			}
+		};
 
 	/*==========================
 	=		INIT FUNCTION      =
 	============================*/
 		$(document).ready(function() {
 			myClass.sliderFunction();
+			myClass.setHeight();
 		});
 	
 		$(window).on('load', function() {});
